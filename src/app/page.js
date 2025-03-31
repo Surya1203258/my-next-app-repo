9fd17bf1057dd86dd63f93ad7f9a8264087f3fb8
TSAstudent <116695459+Surya1203258@users.noreply.github.com>
2025-03-30 22:20:15 -0500

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,9 +15,9 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.js</code>.
+            blah blah blah <code>src/app/page.js</code>.
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Debate.</li>
         </ol>
 
         <div className={styles.ctas}>
@@ -34,7 +34,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            This is Surya 
           </a>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
